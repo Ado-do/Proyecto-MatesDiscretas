979d01e0a2bd67edf5ea19fc792b26edf59a60ef
Adodo <alonbustos2021@udec.cl>
2023-12-05 02:37:20 -0300

    Bosquejo de clases de grafo

--- source/Graph.cpp
+++ source/Graph.cpp
@@ -1,9 +1,24 @@
 #include "Graph.hpp"
 
-void Graph::addNode(int id, std::string name) {
-
+//* METODOS PUBLICOS *//
+ConceGraph::ConceGraph() {
+    // Ingresa los vertices y aristas del grafo de Concepcion
 }
 
-void Graph::addEdge(int node1, int node2, int weight) {
-    
+ConceGraph::~ConceGraph() {
+    // Liberar memoria de los vertices y aristas
+}
+
+//* METODOS PRIVADOS *//
+void ConceGraph::print() {
+
+}
+void ConceGraph::addVertex(int id, std::string label) {
+    verticesMap[id] = new Vertex(id, label);
+}
+
+void ConceGraph::addEdge(int v1_id, int v2_id, int dist) {
+    Edge* edge = new Edge(v1_id, v2_id, dist);
+    verticesMap[v1_id]->incidentEdges.push_back(edge);
+    verticesMap[v2_id]->incidentEdges.push_back(edge);
 }
--- source/Graph.hpp
+++ source/Graph.hpp
@@ -4,59 +4,33 @@
 #include <string>
 #include <map>
 
-// Arco
-struct Edge { 
-    int node1; // Punte de salida
-    int node2; // Punto de llegada
-    int weight; // Distancia entre los dos puntos (metros)
-
-    Edge(int node1, int node2, int weight) : node1(node1), node2(node2), weight(weight) {}
+//* Arista (Calle)
+struct Edge {
+    int v1_id, v2_id; // Identificadores de los vertices (intersecciones) que conecta la arista (calle)
+    int distance; // Distancia entre los dos puntos (metros)
+    Edge(int v1, int v2, int dist) : v1_id(v1), v2_id(v2), distance(dist) {}
 };
 
-// Nodo
-struct Node {
-    std::string name;
-    std::vector<Edge> edges;
-
-    Node(std::string name) : name(name) {}
+//* Vertice (Interseccion de calles)
+struct Vertex {
+    int id; // Identificador del vertice (interseccion)
+    std::string label; // Etiqueta del vertice (nombre de la interseccion)
+    std::vector<Edge*> incidentEdges; // Aristas incidentes al vertice (calles que conectan a la interseccion)
+    Vertex(int id, std::string label) : id(id), label(label) {}
 };
 
-// Grafo
-class Graph {
-private:
-    std::map<int, Node> nodes;
+//* Grafo
+class ConceGraph {
+    std::map<int, Vertex*> verticesMap; // Contenedor de vertices (mapa de intersecciones)
+    // std::vector<Edge*> edgesList; // Vector de aristas (calles)
+
+    void addVertex(int id, std::string label);
+    void addEdge(int v1_id, int v2_id, int dist);
 
 public:
-    void addNode(int id, std::string name);
-    void addEdge(int node1, int node2, int weight);
+    ConceGraph();
+    ~ConceGraph();
 
     void print();
-
-    // Dijkstra
-    std::vector<int> dijkstra(int start, int end);
+    std::vector<int> getShortestPath(std::string start, std::string end); // Dijkstra
 };
-
-/*
-Common algorithms for graph traversal include:
-
-Depth-First Search (DFS): This algorithm explores as far as possible along each branch before backtracking.
-
-Breadth-First Search (BFS): This algorithm explores all the vertices at the present depth before going to the next level.
-
-Dijkstra's Algorithm: This algorithm finds the shortest path from a starting node to all other nodes in the graph. It's a greedy algorithm that always chooses the node with the smallest distance.
-
-Bellman-Ford Algorithm: This algorithm also finds the shortest path from a starting node to all other nodes. Unlike Dijkstra's, it can handle graphs with negative weight edges.
-
-Floyd-Warshall Algorithm: This algorithm finds the shortest paths between all pairs of nodes. It's a dynamic programming algorithm.
-
-A* Search Algorithm: This algorithm finds the shortest path from a starting node to a goal node. It uses a heuristic to estimate the cost to reach the goal, which makes it more efficient than Dijkstra's for many real-world problems.
-
-Here are some suggestions for your next steps:
-
-Implement the DFS and BFS algorithms for graph traversal.
-Implement Dijkstra's algorithm for shortest path finding.
-Test your implementations with some example graphs.
-If your graph could have negative weight edges, consider implementing the Bellman-Ford algorithm.
-If you need to find shortest paths between all pairs of nodes, consider implementing the Floyd-Warshall algorithm.
-If you have a specific goal node and a good heuristic, consider implementing the A* search algorithm.
-*/
